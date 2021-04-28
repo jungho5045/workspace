7af681e064022914633ab5ba011bf0750114553d
Jungho <jungho5045@naver.com>
2021-04-28 16:18:30 +0900

    ByteBuddy를 이용하여 classloader-sample프로젝트에 있는 Masulsa.class에 바이트 코드를 변환시킨다.

--- classloadersample/classloader-sample/src/main/java/me/whiteship/Masulsa.java
+++ classloadersample/classloader-sample/src/main/java/me/whiteship/Masulsa.java
@@ -1,30 +1,8 @@
 package me.whiteship;
 
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.dynamic.ClassFileLocator;
-import net.bytebuddy.implementation.FixedValue;
-import net.bytebuddy.pool.TypePool;
-
-import java.io.File;
-import java.io.IOException;
-
-import static net.bytebuddy.matcher.ElementMatchers.named;
-
 public class Masulsa {
 
     public static void main(String[] args){
-        ClassLoader classLoader = Masulsa.class.getClassLoader();
-        TypePool typePool = TypePool.Default.of(classLoader);
-        try {
-            new ByteBuddy().redefine(
-                    typePool.describe("me.whiteship.Moja").resolve(),
-                    ClassFileLocator.ForClassLoader.of(classLoader))
-                    .method(named("pullOut")).intercept(FixedValue.value("Rebbit!"))
-                    .make().saveIn(new File("C:\\Users\\Jungho\\IdeaProjects\\whiteship\\classloadersample\\classloader-sample\\target\\classes\\"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         System.out.println(new Moja().pullOut());
     }
 }
