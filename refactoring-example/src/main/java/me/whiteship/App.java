47f8f239b833d1468052ede2af518d3872567d6a
Jungho <jungho5045@naver.com>
2021-05-11 13:49:07 +0900

    refactoring-example

--- refactoring-example/src/main/java/me/whiteship/App.java
+++ refactoring-example/src/main/java/me/whiteship/App.java
@@ -4,10 +4,14 @@ package me.whiteship;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        // 타입을 통할 때는
+        Class<Book> bookClass = Book.class;
+
+        // 인스턴스를 이용할 때는 getClass()
+        Book book = new Book();
+        Class<? extends Book> aClass = book.getClass();
+
     }
 }
