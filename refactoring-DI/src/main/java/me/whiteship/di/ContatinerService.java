a1623bf39de6a035ca174af18eccc445a9d9ae59
Jungho <jungho5045@naver.com>
2021-05-28 14:47:27 +0900

    나만의 DI 프레임워크 만들기

--- refactoring-DI/src/main/java/me/whiteship/di/ContatinerService.java
+++ refactoring-DI/src/main/java/me/whiteship/di/ContatinerService.java
@@ -2,6 +2,8 @@ package me.whiteship.di;
 
 public class ContatinerService {
 
+    // 1. getObject는 객체를 제네릭 타입으로 받아 객체를 return 하는 getObject를 만든다.
+    // 2. TestCode에서는 SourceCode를 참조 할 수 있지만, 반대로 SourceCode에서는 TestCode를 참조할 수 없다.
     public static <T> T getObject (Class<T> classType){
         return null;
     }
--- refactoring-DI/src/test/java/me/whiteship/di/ContatinerServiceTest.java
+++ refactoring-DI/src/test/java/me/whiteship/di/ContatinerServiceTest.java
@@ -1,7 +1,15 @@
 package me.whiteship.di;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ContatinerServiceTest {
+
+    @Test
+    public void getObject() {
+        BookRepository bookRepository = ContatinerService.getObject(BookRepository.class);
+        assertNotNull(bookRepository);
+    }
 
 }
