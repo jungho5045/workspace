1debffacde6d92b540c0f29eef6d862e1e35e82c
Jungho <jungho5045@naver.com>
2021-05-31 15:13:52 +0900

    나만의 DI 프레임워크 만들기

--- refactoring-DI/src/main/java/me/whiteship/di/ContatinerService.java
+++ refactoring-DI/src/main/java/me/whiteship/di/ContatinerService.java
@@ -1,11 +1,35 @@
 package me.whiteship.di;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class ContatinerService {
 
-    // 1. getObject는 객체를 제네릭 타입으로 받아 객체를 return 하는 getObject를 만든다.
+    // 1. <T> type은 제네릭 타입으로 메소드를 리턴할 때 Object type으로 나오는 것이 아니라 매스드 파라매터로 넘겨주는 class type으로 리턴을 해준다.
     // 2. TestCode에서는 SourceCode를 참조 할 수 있지만, 반대로 SourceCode에서는 TestCode를 참조할 수 없다.
     public static <T> T getObject (Class<T> classType){
-        return null;
+        T instance = createInstance(classType);
+        Arrays.stream(classType.getDeclaredFields()).forEach(f -> {
+            if (f.getAnnotation(Inject.class) != null){
+                Object fieldinstance = createInstance(f.getType());
+                f.setAccessible(true);
+                try {
+                    f.set(instance, fieldinstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        return instance;
+    }
+
+    private static <T> T createInstance(Class<T> classType){
+        try {
+            return classType.getConstructor(null).newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- refactoring-DI/src/test/java/me/whiteship/di/ContatinerServiceTest.java
+++ refactoring-DI/src/test/java/me/whiteship/di/ContatinerServiceTest.java
@@ -2,14 +2,21 @@ package me.whiteship.di;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class ContatinerServiceTest {
 
     @Test
-    public void getObject() {
+    public void getObject_BookRepository() {
         BookRepository bookRepository = ContatinerService.getObject(BookRepository.class);
         assertNotNull(bookRepository);
+    }
+
+    @Test
+    public void getObject_BookService() {
+        BookService bookService = ContatinerService.getObject(BookService.class);
+        assertNotNull(bookService);
+        assertNotNull(bookService.bookRepository);
     }
 
 }
